66f8c4e3d86646d4d345e740c90dd551aa6b6b5d
SoggyTony <anthonymenendez16@gmail.com>
2024-04-12 00:05:58 -0400

    created HangManPlayer to implement Guesser class

--- src/Guesser.java
+++ src/Guesser.java
@@ -8,55 +8,54 @@ import java.util.LinkedList;
 public class Guesser {
    final String word;
 
-   private Guesser (final String word) {
+   private Guesser(final String word) {
       this.word = word;
    }
 
-   public static void main (final String[] args) throws IOException {
-      final long startTime = System.nanoTime ();
+   public static void main(final String[] args) throws IOException {
+      final long startTime = System.nanoTime();
       PartitionByLength.run("words.txt");
-      System.out.println (run (args[0].toLowerCase ()));
-      System.out.println (System.nanoTime () - startTime);
+      System.out.println(run(args[0].toLowerCase()));
+      System.out.println(System.nanoTime() - startTime);
    }
 
    /*
     * 0) Get set with words of right length
-    * 1) Analyze set for letter that has not been guessed yet and is in the most words
+    * 1) Analyze set for letter that has not been guessed yet and is in the most
+    * words
     * 2) Guess letter -> get word with guessed letter in positions
     * 3) Shrink set exclude words which do not have the guessed letter in positions
     * 4) Repeat from 1
     */
-   public static boolean run (final String word) throws IOException {
-      final StringBuilder sequence = new StringBuilder (word.length ());
-      sequence.append (word.length ());
+   public static boolean run(final String word) throws IOException {
+      final StringBuilder sequence = new StringBuilder(word.length());
+      sequence.append(word.length());
       int strikes = 0;
 
-      final Guesser guesser = new Guesser (word);
-      HomogenousWordSet possibleWords =
-            MatchWordstoPattern.run (Integer.toString (word.length ()));
+      final Guesser guesser = new Guesser(word);
+      HomogenousWordSet possibleWords = MatchWordstoPattern.run(Integer.toString(word.length()));
 
       while (true) {
-         //System.out.println (possibleWords);
-         final char guess =
-               CharacterDistributionAnalyzer.getNext (possibleWords, sequence.toString ());
+         // System.out.println (possibleWords);
+         final char guess = CharacterDistributionAnalyzer.getNext(possibleWords, sequence.toString());
 
-         //System.out.println (guess);
+         // System.out.println (guess);
 
          if (possibleWords.occupancy == 0) {
             return false;
          }
 
          if (guess < 'a') {
-            System.out.println (sequence.toString ());
-            System.out.println (possibleWords);
+            System.out.println(sequence.toString());
+            System.out.println(possibleWords);
             return true;
          }
 
-         sequence.append (guess);
+         sequence.append(guess);
 
-         final var positions = guesser.checkGuess (guess);
+         final var positions = guesser.checkGuess(guess);
 
-         if (positions.size () == 0) {
+         if (positions.size() == 0) {
             strikes++;
 
             if (strikes >= 6) {
@@ -64,24 +63,18 @@ public class Guesser {
             }
          }
 
-         sequence.append (positions.size ());
+         sequence.append(positions.size());
 
-         possibleWords = MatchWordstoPattern.runPositionsForLetterOnSet (positions, guess,
+         possibleWords = MatchWordstoPattern.runPositionsForLetterOnSet(positions, guess,
                possibleWords);
       }
    }
 
-   public char guess (final String sequence) throws IOException {
-      System.out.println (sequence);
-      final var possibleSet = MatchWordstoPattern.run (sequence);
-      return CharacterDistributionAnalyzer.getNext (possibleSet, sequence);
-   }
-
-   public LinkedList<Integer> checkGuess (final char guess) {
-      final LinkedList<Integer> presentIndices = new LinkedList<> ();
-      for (int i = 0; i < word.length (); i++) {
-         if (word.charAt (i) == guess) {
-            presentIndices.add (i);
+   public LinkedList<Integer> checkGuess(final char guess) {
+      final LinkedList<Integer> presentIndices = new LinkedList<>();
+      for (int i = 0; i < word.length(); i++) {
+         if (word.charAt(i) == guess) {
+            presentIndices.add(i);
          }
       }
       return presentIndices;
--- src/HangmanPlayer.java
+++ src/HangmanPlayer.java
@@ -12,41 +12,69 @@
 
 */
 
+import java.io.IOException;
+import java.util.LinkedList;
 
-public class HangmanPlayer
-{
+public class HangmanPlayer {
+  StringBuilder sequence;
+  HomogenousWordSet possibleWords;
+  char guess;
 
-    // initialize HangmanPlayer with a file of English words
-    public HangmanPlayer(String wordFile)
-    {
+  // initialize HangmanPlayer with a file of English words
+  public HangmanPlayer(String wordFile) {
+    try {
+      PartitionByLength.run(wordFile);
+    } catch (IOException e) {
+      System.out.println("FUCK");
+      System.exit(-1);
+    }
+  }
 
+  // based on the current (partial or intitially blank) word
+  // guess a letter
+  // currentWord: current word, currenWord.length has the length of the hidden
+  // word
+  // isNewWord: indicates a new hidden word
+  // returns the guessed letter
+  // assume all letters are in lower case
+  public char guess(String currentWord, boolean isNewWord) {
+
+    if (isNewWord) {
+      // System.out.println();
+      sequence = new StringBuilder(currentWord.length());
+      sequence.append(currentWord.length());
+      try {
+        possibleWords = MatchWordstoPattern.run(Integer.toString(currentWord.length()));
+      } catch (IOException e) {
+        System.out.println("FUCK");
+        System.exit(-1);
+      }
     }
 
-    // based on the current (partial or intitially blank) word
-    //    guess a letter
-    // currentWord: current word, currenWord.length has the length of the hidden word
-    // isNewWord: indicates a new hidden word
-    // returns the guessed letter
-    // assume all letters are in lower case
-    public char guess(String currentWord, boolean isNewWord)
-    {
-	char guess = ' ';
-	
-        return guess;
+    guess = CharacterDistributionAnalyzer.getNext(possibleWords, sequence.toString());
+
+    sequence.append(guess);
+    return guess;
+  }
+
+  // feedback on the guessed letter
+  // isCorrectGuess: true if the guessed letter is one of the letters in the
+  // hidden word
+  // currentWord: partially filled or blank word
+  //
+  // Case isCorrectGuess currentWord
+  // a. true partial word with the guessed letter
+  // or the whole word if the guessed letter was the
+  // last letter needed
+  // b. false partial word without the guessed letter
+  public void feedback(boolean isCorrectGuess, String currentWord) {
+    // System.out.println("Current Word: " + currentWord);
+    final LinkedList<Integer> presentIndices = new LinkedList<>();
+    for (int i = 0; i < currentWord.length(); i++) {
+      if (currentWord.charAt(i) == guess) {
+        presentIndices.add(i);
+      }
     }
-
-    // feedback on the guessed letter
-    // isCorrectGuess: true if the guessed letter is one of the letters in the hidden word
-    // currentWord: partially filled or blank word
-    //   
-    // Case       isCorrectGuess      currentWord   
-    // a.         true                partial word with the guessed letter
-    //                                   or the whole word if the guessed letter was the
-    //                                   last letter needed
-    // b.         false               partial word without the guessed letter
-    public void feedback(boolean isCorrectGuess, String currentWord)
-    {
-
-    }
-
+    possibleWords = MatchWordstoPattern.runPositionsForLetterOnSet(presentIndices, guess, possibleWords);
+  }
 }
