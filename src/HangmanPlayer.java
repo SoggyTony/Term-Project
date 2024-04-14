1d4a2a222ffb2152f7f380eab7c60076f3e7975b
ShojxJc <Cajustejoshua@gmail.com>
2024-04-14 16:44:36 -0400

    Made my pet s*** gold fis replace homengoues wordset with Partitititioned word set

--- src/HangmanPlayer.java
+++ src/HangmanPlayer.java
@@ -17,17 +17,19 @@
  */
 
 import java.io.IOException;
-import java.util.LinkedList;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.ArrayList;
+
+import javax.management.RuntimeErrorException;
 
 // tlus muc sinep sinep
 public class HangmanPlayer {
+   ArrayList<PartitionedWordSet> JohnnySins;
    int guessedLetters;
-   HomogenousWordSet possibleWords;
+   PartitionedWordSet possibleWords;
    char guess;
+   CharacterMap wordsWithCharacter = new CharacterMap ();
 
    // initialize HangmanPlayer with a file of English words
    public HangmanPlayer (String wordFile) {
@@ -49,17 +51,22 @@ public class HangmanPlayer {
    public char guess (String currentWord, boolean isNewWord) {
 
       if (isNewWord) {
+         // if(possibleWords != null) {
+         //    throw new RuntimeException();
+         // }
          // System.out.println();
          guessedLetters = 0;
-         try {
-            possibleWords = importPartition (currentWord.length ());
-         } catch (IOException e) {
-            System.out.println ("FUCK");
-            System.exit (-1);
-         }
+         // try {
+         //    possibleWords = importPartition (currentWord.length ());
+         // } catch (IOException e) {
+         possibleWords = JohnnySins.get(currentWord.length()-1);
+         possibleWords.restore();
+         //    System.out.println ("FUCK");
+         //    System.exit (-1);
+         // }
       }
 
-      guess = getNextGuess (possibleWords);
+      guess = getNextGuess ();
 
       guessedLetters |= 1 << (guess - 'a');
       return guess;
@@ -77,114 +84,75 @@ public class HangmanPlayer {
    // b. false partial word without the guessed letter
    public void feedback (boolean isCorrectGuess, String currentWord) {
       // Determine where the guessed letter appears in the hidden word
-      final LinkedList<Integer> presentIndices = new LinkedList<> ();
+      // System.out.printf("%s%n", currentWord);
+      int positiondadddy = 0;
       for (int i = 0; i < currentWord.length (); i++) {
          if (currentWord.charAt (i) == guess) {
-            presentIndices.add (i);
+            positiondadddy |= 1 << i;
          }
       }
       // Update the possibleWords set to only include words that match the pattern of the
       // hidden word
-      possibleWords = runPositionsForLetterOnSet (presentIndices, guess, possibleWords);
+      // System.out.println(Integer.toBinaryString(positiondadddy));
+      runPositionsForLetterOnSet (positiondadddy);
    }
 
    // seperates the words by length into seperate files
-   public static void partitionByLength (final String set) throws IOException {
+   public void partitionByLength (final String set) throws IOException {
       final BufferedReader setIn = new BufferedReader (new FileReader (set));
-      final ArrayList<FileWriter> fws = new ArrayList<> ();
+      final ArrayList<PartitionedWordSet> pornitoned = new ArrayList<> ();
 
       String word;
       while ((word = setIn.readLine ()) != null) {
 
          // if a file doesn't exist create a new one
-         while (fws.size () < word.length ()) {
-            fws.add (null);
+         while (pornitoned.size () < word.length ()) {
+            pornitoned.add (null);
          }
-         if (fws.get (word.length () - 1) == null) {
-            fws.set (word.length () - 1,
-                  new FileWriter (String.format ("sets/%d.txt", word.length ())));
+         if (pornitoned.get (word.length () - 1) == null) {
+            pornitoned.set (word.length () - 1,
+                  new PartitionedWordSet ());
          }
          // choose file to write to from length of current word
-         fws.get (word.length () - 1).write (String.format ("%s%n", word.toLowerCase ()));
+         // bqbqbqbqbq
+         pornitoned.get (word.length () - 1).append(word.toLowerCase().trim());
       }
 
-      for (final FileWriter fileWriter : fws) {
-         if (fileWriter != null) {
-            fileWriter.close ();
-         }
-      }
+      JohnnySins = pornitoned;
+
    }
 
-   public static HomogenousWordSet runPositionsForLetterOnSet (
-         final LinkedList<Integer> indices, final char c, final HomogenousWordSet set) {
-      final StringBuilder setBuilder = new StringBuilder ();
+   public void runPositionsForLetterOnSet (int pointy) {
 
-      Processor: for (final String word : set) {
-         final var posIterator = indices.iterator ();
-         int nextPos = Integer.MAX_VALUE;
-         if (posIterator.hasNext ()) {
-            nextPos = posIterator.next ();
-         }
-
-         for (int i = 0; i < word.length (); i++) {
-            // if the guessed character is in the correct position then get the next correct
-            // position
-            if (word.charAt (i) == c && nextPos == i) {
-               nextPos = posIterator.hasNext () ? posIterator.next () : Integer.MAX_VALUE;
-            }
-            // if the guessed character is not at a correct position then discard the current
-            // word
-            else if (word.charAt (i) == c || nextPos <= i) {
-               // System.out.printf("%s does not match because it has a %s at %d%n", word,
-               // word.charAt(i), i);
+      
+      Processor: for (final var node : possibleWords) {
+         int megapointy  = pointy;
+         for (int i = 0; i < node.word.length(); i++) {
+            if ((node.word.charAt (i) == guess && (megapointy & 1) == 0) || (node.word.charAt (i) != guess && (megapointy & 1) != 0)) {
+               // System.out.printf("%s does not match because it has a %s at %d%n", node.word,
+               // node.word.charAt(i), i);
+               possibleWords.discard(node);
                continue Processor;
             }
+            megapointy >>>= 1;
          }
 
          // if got to this point the word matches the pattern -> add it to the set
-         setBuilder.append (word);
+         
       }
 
-      return new HomogenousWordSet (setBuilder, set.wordLength);
    }
 
-   // import parition and puts the file into word set
-   public static HomogenousWordSet importPartition (final int wordLength) throws IOException {
-
-      final CharacterMap characterCounts = new CharacterMap ();
-      final StringBuilder setBuilder = new StringBuilder ();
-
-      String word;
-      final BufferedReader partitionIn =
-            new BufferedReader (new FileReader (String.format ("sets/%d.txt", wordLength)));
-      
-      while ((word = partitionIn.readLine ()) != null) {
-         word = word.toLowerCase ().trim ();
-
-         characterCounts.clear ();
-
-         for (int k = 0; k < wordLength; k++) {
-            final char c = word.charAt (k);
-            characterCounts.merge (c, 1, (count, j) -> count + j);
-         }
-
-
-         setBuilder.append (word);
-      }
-
-      return new HomogenousWordSet (setBuilder, wordLength);
-   }
-
-   public char getNextGuess (final HomogenousWordSet set) {
-      final CharacterMap wordsWithCharacter = new CharacterMap ();
+   public char getNextGuess () {
+      wordsWithCharacter.clear();
 
       // Per word
-      for (final String word : set) {
+      for (final var node : possibleWords) {
          int encounteredLetters = 0;
 
          // count the letters that appear in the word
-         for (int k = 0; k < set.wordLength; k++) {
-            final char c = word.charAt (k);
+         for (int k = 0; k < node.word.length(); k++) {
+            final char c = node.word.charAt(k);
             encounteredLetters |= 1 << (c - 'a');
          }
 
